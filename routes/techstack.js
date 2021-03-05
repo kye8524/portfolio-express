f290058811299d018e388e187ed9d1a9db5b0c39
kye8524 <kye8314@gmail.com>
2021-03-05 18:09:31 +0900

    데이터베이스 수정

--- routes/techstack.js
+++ routes/techstack.js
@@ -26,11 +26,51 @@ const pool = require('../pool')
  */
 router.get('/', async (req,res,next) => {
         try{
-            const data = await pool.query('select * from TechStack')
+            const data = await pool.query('select * from about_tech')
             return res.json(data[0])
         }catch (err){
             return  res.status(400).json(err)
         }
-    })
+})
+/**
+ * @swagger
+ * /tech/pro:
+ *   post:
+ *     summary: 기슬 스택 정보 입력하기
+ *     tags: [tech-stack]
+ *     consumes:
+ *       - application/x-www-form-urlencoded
+ *     requestBody:
+ *       content:
+ *          application/x-www-form-urlencoded:
+ *              schema:
+ *                  type: object
+ *                  properties:
+ *                      name:
+ *                          type: string
+ *                      stack:
+ *                          type: int
+ *              required:
+ *                  - name
+ *                  - stack
+ *     responses:
+ *       200:
+ *         description: 성공
+ *       403:
+ *         $ref: '#/components/res/Forbidden'
+ *       404:
+ *         $ref: '#/components/res/NotFound'
+ *       400:
+ *         $ref: '#/components/res/BadRequest'
+ */
+router.post('/pro', async (req,res,next) => {
+        try{
+            let {name, stack}=req.body;
+            const data = await pool.query('INSERT INTO about_tech (name, stack) values(?,?)',[name,stack])
+            return res.json(data[0])
+        }catch (err){
+            return  res.status(400).json(err)
+        }
+})
 
 module.exports = router;
